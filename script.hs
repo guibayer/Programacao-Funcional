0b4bbd95c6e49510dfad42430a141f2d6bc2bea5
guibayer <guibayer2@gmail.com>
2019-03-13 18:11:04 -0300

    Update script.hs

--- script.hs
+++ script.hs
@@ -17,7 +17,28 @@ igual x y =     x == y
 tresIguais :: Int -> Int -> Int -> Bool
 tresIguais x y z = (x == y) && (y == z)
 
+vendas ::  Int -> Int
+vendas 0 = 5
+vendas 1 = 5
+
 vendaTotal :: Int -> Int
 vendaTotal n
  | n == 0      = vendas 0
  | otherwise   = vendas n + vendaTotal (n-1)
+
+palindrome :: String -> Bool
+palindrome s = s == reverse s
+
+n :: Int
+n = 0
+
+--Defina a funcaoquantosSaoIguais :: Int -> Int -> Int -> Int que conta quantos argumentos iguais a funcao recebeu
+qtdeIguais :: Int -> Int -> Int -> Int
+qtdeIguais x y z
+ | ( x == y) && ( y == z) && ( x == z) = 3
+ | ( x == y) || ( y == z) || ( x == z) = 2
+ | otherwise = 0
+
+ -- Defina a funcao
+todosDiferentes :: Int -> Int -> Int -> Bool
+todosDiferentes x y z = ( x /= y) && ( y /= z) && ( x /= z)
